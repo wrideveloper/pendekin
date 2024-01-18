9312d4243bd38ed954039acab69a7fa3044b9178
elianiva <dicha.arkana03@gmail.com>
2024-01-18 15:12:20 +0700

    feat: qrcode support

--- islands/ShortenedModal.tsx
+++ islands/ShortenedModal.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useRef } from "preact/hooks"
 import { useSignal } from "@preact/signals"
+import qrGenerator from "qrcode-generator";
+import domToImage from "dom-to-image";
 import { CopyIcon } from "../components/icons/ic_copy.tsx";
 import { EditIcon } from "../components/icons/ic_edit.tsx";
 import { SaveIcon } from "../components/icons/ic_save.tsx";
@@ -12,15 +14,34 @@ type ShortenedModalProps = {
 
 export default function ShortenedModal(props: ShortenedModalProps) {
 	const dialogRef = useRef<HTMLDialogElement>(null);
+	const qrContainerRef = useRef<HTMLDivElement>(null);
 	const isEditable = useSignal(false);
 	const url = useSignal<string>(props.url ?? "");
 	const hasCopied = useSignal(false);
 
 	useEffect(() => {
 		if (dialogRef.current === null) return;
+		if (qrContainerRef.current === null) return;
 		if (!props.url) return;
 		dialogRef.current.showModal();
+
+		const qrCode = qrGenerator(0, "M");
+		qrCode.addData(`https://s.wridev.id/${props.url}`);
+		qrCode.make();
+		qrContainerRef.current.innerHTML = qrCode.createSvgTag({
+			scalable: true,
+			margin: 0,
+		});
 	}, [props.url]);
+
+	async function downloadQrCode() {
+		const qrCode = document.getElementById("qr-code") as HTMLDivElement;
+		const imageResult = await domToImage.toPng(qrCode);
+		const link = document.createElement("a");
+		link.download = `s.wridev.id-${props.url}.png`;
+		link.href = imageResult;
+		link.click();
+	}
 
 	return (
 		<dialog
@@ -29,9 +50,31 @@ export default function ShortenedModal(props: ShortenedModalProps) {
 			ref={dialogRef}
 		>
 			<div className="modal-box">
-				<h3 className="font-bold text-lg mb-4">
+				<h3 className="font-bold text-2xl mb-4">
 					{props.isUpdated ? "Your link has been updated!" : "Your link is ready!"}
 				</h3>
+				<div class="mx-auto mb-4 max-w-fit">
+					<div
+						id="qr-code"
+						class="relative p-4 border border-slate-200 rounded-lg max-w-fit bg-white"
+					>
+						<div
+							ref={qrContainerRef}
+							class="w-64 h-64"
+						/>
+						<img
+							src="/assets/wri-logo-squared.png"
+							class="absolute w-20 h-20 p-2 rounded-lg bg-white left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+						/>
+					</div>
+					<a
+						class="block text-sm underline text-slate-500 pt-2 outline-none"
+						href="javascript:void(0);"
+						onClick={downloadQrCode}
+					>
+						Download QR Code
+					</a>
+				</div>
 				<form
 					class="flex justify-between items-center gap-2 border border-slate-200 rounded-lg p-2"
 					method="POST"
